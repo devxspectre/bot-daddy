000c9321fb9cad1fe98c428e9d346cfaa79b2b05
Sourav6971 <mailsourav014@gmail.com>
2025-12-24 21:46:15 +0530

    build

--- apps/bot-api/index.ts
+++ apps/bot-api/index.ts
@@ -4,15 +4,19 @@ import routes from "./routes";
 import { initDatabase } from "./services";
 
 import cors from "cors";
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+  return res.json({
+    message:'Server running healthy'
+  })
+})
+
 app.use('/api/v1/',routes)
 
-// Initialize database and start server
 initDatabase()
   .then(() => {
     app.listen(PORT, () => {
@@ -23,3 +27,6 @@ initDatabase()
     console.error("Failed to initialize database:", err);
     process.exit(1);
   });
+
+
+export default app
